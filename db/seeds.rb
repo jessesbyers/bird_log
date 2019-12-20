8453ac133f3ec1d0dbd0c94511627538cf826eb6
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-20 15:11:48 +0000

    changed date format in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,14 @@ Bird.create(Bird.scrape_attributes("https://www.audubon.org/field-guide/bird/bla
 
 
 sighting_list = [
-  ["https://www.audubon.org/field-guide/bird/bald-eagle", "01/11/2017", "Back yard", "Soaring overhead", "1", "1"],
-  ["https://www.audubon.org/field-guide/bird/bald-eagle", "05/18/2017", "Front yard", "Flying south", "2", "1"],
-  ["https://www.audubon.org/field-guide/bird/bald-eagle", "01/20/2019", "Bay Park", "Sitting on dead tree branch", "3", "1"],
-  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "08/11/2017", "Front garden", "sipping nectar", "4", "2"],
-  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "07/02/2017", "Day Lily patch", "2 birds observed feeding", "5", "2"],
-  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "01/30/2018", "Back deck", "3 birds at feeder", "1", "2"],
-  ["https://www.audubon.org/field-guide/bird/bald-eagle", "05/18/2017", "Kamp Kill Kare", "Sitting on dead tree branch", "2", "1"],
-  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "04/18/2019", "Front porch", "At hummingbird feeder", "1", "2"]
+  ["https://www.audubon.org/field-guide/bird/bald-eagle", "2017-01-11", "Back yard", "Soaring overhead", "1", "1"],
+  ["https://www.audubon.org/field-guide/bird/bald-eagle", "2017-05-18", "Front yard", "Flying south", "2", "1"],
+  ["https://www.audubon.org/field-guide/bird/bald-eagle", "2019-01-20", "Bay Park", "Sitting on dead tree branch", "3", "1"],
+  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "2017-08-11", "Front garden", "sipping nectar", "4", "2"],
+  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "2017-05-04", "Day Lily patch", "2 birds observed feeding", "5", "2"],
+  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "2018-01-30", "Back deck", "3 birds at feeder", "1", "2"],
+  ["https://www.audubon.org/field-guide/bird/bald-eagle", "2017-05-18", "Kamp Kill Kare", "Sitting on dead tree branch", "2", "1"],
+  ["https://www.audubon.org/field-guide/bird/black-chinned-hummingbird", "2019-04-18", "Front porch", "At hummingbird feeder", "1", "2"]
 ]
 
 sighting_list.each do |audubon_url, date, location, notes, user_id, bird_id|
